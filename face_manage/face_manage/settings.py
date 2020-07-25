67648f79f883125860ef7370ffbcdab8776f3525
whx1 <whx123@163.com>
2020-07-25 20:55:51 +0800

    abc

--- face_manage/face_manage/settings.py
+++ face_manage/face_manage/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j4fgqx194)@rep16&c4so^g6myxy3)_3b%_k1&n^zozwq$*3nh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
