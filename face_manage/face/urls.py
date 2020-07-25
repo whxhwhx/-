aaae502ab32a1e473311d4dcd30bdc1d8cc26056
whx1 <whx123@163.com>
2020-07-25 21:33:00 +0800

    jiben

--- face_manage/face/urls.py
+++ face_manage/face/urls.py
@@ -7,13 +7,12 @@ urlpatterns = [
     url(r'^login/', views.login),
     url(r'^login_handle/', views.login_handle),
     url(r'user_list/(\d+)/', views.user_list),
-    url(r'visitor_list/', views.visitor_list),
-    url(r'dangerous_list/', views.dangerous_list),
+    url(r'visitor_list/(\d+)/', views.visitor_list),
+    url(r'dangerous_list/(\d+)', views.dangerous_list),
     url(r'logout', views.logout),
     url(r'detail/(\d+)/', views.detail),
     url(r'upload_img', views.upload_img),
     url(r'console/', views.console_index),
     url(r'^about/', views.about),
     url(r'^contact/', views.contact),
-
 ]
